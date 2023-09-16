e12b2012843cbb539caa069f001f889a5722364b
Makon <makon@hey.com>
2023-09-16 11:07:22 -0600

    run unit tests on pr

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,7 @@
-import { configDefaults, defineConfig } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     dir: "./tests/unit",
-    include: ["**/*.{test}.?(c|m)[jt]s?(x)"],
-    exclude: [
-      ...configDefaults.exclude,
-      "./tests/component/**",
-      "./tests/e2e/**",
-    ],
   },
 });
